48ace5db57d3132a518fee695f81b2efaad7324a
HyukwooKwon <kkangidang@naver.com>
2025-03-12 20:36:58 +0900

    1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Chatbot from './components/Chatbot';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/chatbot" replace />} />
         <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </Router>
